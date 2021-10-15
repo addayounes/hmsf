feb4c32925178ee7010f0c10acb3e183952c24a0
addy <addayounes8@gmail.com>
2021-10-15 12:28:49 +0100

    Added (add to favorite) to product details page

---
+++ src/components/AddFavorite/AddFavorite.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { addToFavorite, removeFromFavorite } from "../../redux/ducks/flowers";
+import { RootState } from "../../redux/store";
+import { ProductCardProps } from "../ProductCard/ProductCard";
+import "./AddFavorite.css";
+
+const AddFavorite: React.FC<ProductCardProps> = (props) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const isLogged = useSelector((state: RootState) => state.userReducer.isLogged);
+
+    const handleFavorite = (): void => {
+        if (isLogged) {
+            props.isFavorite
+                ? dispatch(removeFromFavorite(props.id))
+                : dispatch(addToFavorite(props));
+        } else history.push("/login");
+    };
+
+    return (
+        <>
+            <FaHeart
+                className={`add-favorite-btn ${props.isFavorite ? "active-favorite" : ""}`}
+                onClick={handleFavorite}
+            />
+        </>
+    );
+};
+
+export default AddFavorite;
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { FaCrown, FaCheck, FaHeart } from "react-icons/fa";
+import { FaCrown, FaCheck } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { addToFavorite, removeFromFavorite } from "../../redux/ducks/flowers";
-import ProductCardType from "../../types/flower";
-import isInCart from "../../utils/isInCart";
 import { addToCart } from "../../redux/ducks/cart";
 import { RootState } from "../../redux/store";
+import ProductCardType from "../../types/flower";
+import isInCart from "../../utils/isInCart";
+import AddFavorite from "../AddFavorite/AddFavorite";
 import "./ProductCard.css";
 
-interface ProductCardProps extends ProductCardType {
+export interface ProductCardProps extends ProductCardType {
     isFavorite: boolean;
 }
 
@@ -17,7 +17,6 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
     const history = useHistory();
     const dispatch = useDispatch();
     const cartItems = useSelector((state: RootState) => state.cartReducer.cartItems);
-    const isLogged = useSelector((state: RootState) => state.userReducer.isLogged);
 
     const handleAddingToCart = (product: ProductCardType): void => {
         if (isInCart(props.id, cartItems)) return;
@@ -26,14 +25,6 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
 
     const showFlowerDetailsPage = (): void => {
         history.push(`/store/${props.id}`);
-    };
-
-    const handleFavorite = (): void => {
-        if (isLogged) {
-            props.isFavorite
-                ? dispatch(removeFromFavorite(props.id))
-                : dispatch(addToFavorite(props));
-        } else history.push("/login");
     };
 
     return (
@@ -47,10 +38,7 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
                     </span>
                 )}
             </div>
-            <FaHeart
-                className={props.isFavorite ? "active-favorite" : ""}
-                onClick={handleFavorite}
-            />
+            <AddFavorite {...props} />
             <div className='product-card-content split-between'>
                 <div className='product-card-infos'>
                     <p>{props.title}</p>
